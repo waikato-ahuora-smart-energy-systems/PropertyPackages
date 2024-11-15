6de422469458f1aa373007f7ccb78c3d1d0f8a64
Mahaki-Leach <ml508@students.waikato.ac.nz>
2024-11-15 15:18:27 +1300

    END of day commit

---
+++ property_packages/tests/test_heater_pr.py
@@ -0,0 +1,31 @@
+# Build and solve a heater block.
+from ..build_package import build_package
+from pytest import approx
+
+# Import objects from pyomo package 
+from pyomo.environ import ConcreteModel, SolverFactory, value, units
+
+# Import the main FlowsheetBlock from IDAES. The flowsheet block will contain the unit model
+from idaes.core import FlowsheetBlock
+from idaes.core.util.model_statistics import degrees_of_freedom
+from idaes.models.unit_models.heater import Heater
+from idaes.core.util.tables import _get_state_from_port
+
+
+
+def test_heater():
+    m = ConcreteModel()
+    m.fs = FlowsheetBlock(dynamic=False) 
+    m.fs.properties = build_package("peng-robinson", ["benzene", "toluene"], ["Liq", "Vap"])
+    
+    m.fs.heater = Heater(property_package=m.fs.properties)
+    m.fs.heater.heat_duty.fix(0)
+    m.fs.heater.inlet.flow_mol.fix(1)
+    m.fs.heater.inlet.enth_mol.fix(1878.87)
+    m.fs.heater.inlet.pressure.fix(101325)
+    assert degrees_of_freedom(m) == 0
+    solver = SolverFactory('ipopt')
+    solver.solve(m, tee=True)
+    assert value(_get_state_from_port(m.fs.heater.outlet,0).temperature) == approx(298)
+    assert value(m.fs.heater.outlet.pressure[0]) == approx(101325)
+    assert value(m.fs.heater.outlet.flow_mol[0]) == approx(1)
