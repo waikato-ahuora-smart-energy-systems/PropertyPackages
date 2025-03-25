ca53c13c29129c55bb697200c2cfa0e7b5d1c645
Stephen Burroughs <stephen.burroughs@waikato.ac.nz>
2025-03-25 16:00:52 +1300

    Added surrogate for Humid Air

--- property_packages/build_package.py
+++ property_packages/build_package.py
@@ -3,6 +3,7 @@ from .helmholtz.helmholtz_builder import build_helmholtz_package
 from property_packages.types import PackageName, States
 from typing import List, Optional
 from property_packages.milk import build_milk_package
+from property_packages.humid_air import build_humid_air_package
 
 def build_package(package_name: PackageName, compound_list: List[str], valid_states: List[States]=["Liq", "Vap"]): # type: ignore
 
@@ -18,5 +19,7 @@ def build_package(package_name: PackageName, compound_list: List[str], valid_sta
             pass # TODO: Implement build_package for nrtl.
         case "milk":
             return build_milk_package(compound_list)
+        case "humic_air":
+            return build_humid_air_package(compound_list)
         case _:
             raise ValueError(f"Invalid package name {package_name}. Expected a valid property package type, e.g helmholtz")
