376f585bc22e7ac540909dc7b603508910c0e8df
Mahaki-Leach <ml508@students.waikato.ac.nz>
2025-08-07 12:49:35 +1200

    added additional tests, added 4/5 loaders

--- compounds/tests/test_search.py
+++ compounds/tests/test_search.py
@@ -6,10 +6,8 @@ def test_compound_search():
 
 def test_compound_search_filter():
     assert len(db.search_compounds("ide", package_filters=["biomass_combustion_reaction"])) == 2
-    assert len(db.search_compounds("milk", package_filters=["peng-robinson"])) == 1 # needs to be 0
-    # assert len(db.search_compounds("milk", package_filters=["milk"])) == 1
-
-
+    assert len(db.search_compounds("milk", package_filters=["peng-robinson"])) == 0
+    assert len(db.search_compounds("milk", package_filters=["milk"])) == 1
 
 def test_compound_search_filter_strict():
     assert db.search_compounds("ide", package_filters=["biomass_combustion_reaction", "peng-robinson"], 
