76b417020f484db84febbafc61a6bf58841b176c
Kai Meiklejohn <kaim.meiklejohn@gmail.com>
2026-03-02 10:25:23 +1300

    logging

--- ahuora_property_packages/surrogate/tests/test_surrogate_model.py
+++ ahuora_property_packages/surrogate/tests/test_surrogate_model.py
@@ -1,4 +1,4 @@
-from ..surrogate_builder import build_surrogate_package
+from ahuora_property_packages.surrogate.surrogate_builder import build_surrogate_package
 from pyomo.environ import ConcreteModel
 from idaes.core import FlowsheetBlock
 from idaes.core.util.model_statistics import degrees_of_freedom
@@ -20,5 +20,15 @@ def test_surrogate_block():
     results = solver.solve(m, tee=True)
     assert results.solver.termination_condition == "optimal"
 
+    print("Post-solve surrogate state block values:")
+    print("Temperature:", m.fs.sb[0].temperature.value)
+    print("Pressure:", m.fs.sb[0].pressure.value)
+    print("Flow Mol:", m.fs.sb[0].flow_mol.value)
+    print("Enthalpy:", m.fs.sb[0].enth_mol.value)
+    print("Entropy:", m.fs.sb[0].entr_mol.value)
+    print("Dynamic Viscosity:", m.fs.sb[0].dynamic_viscosity.value)
+    print("Kinematic Viscosity:", m.fs.sb[0].kinematic_viscosity.value)
+    print("Molar Mass:", m.fs.sb[0].flow_mass.value)
+    print("Specific Volume:", m.fs.sb[0].specific_volume.value)
 
 
